92c16e7776dc05e3ca249c7d0d4a5de79505b1ca
João Pedro <joao.26100698ead@alunos.unisagrado.edu.br>
2026-09-13 20:51:57 -0300

    Interações de usuario por Javascript

--- js/prods.js
+++ js/prods.js
@@ -1,39 +1,32 @@
-function produtos(escolha){
+function produtos(escolha){ //Usado em index.html para escolher o animal
     var urlPad = window.location.href+"produtos.html"
     var urlMod = new URL(urlPad);
     urlMod.searchParams.set("animal", escolha);
     window.location.href = urlMod.href;
-}
+} 
 
 function retornaInicio(){
     window.location.href = "../"
 }
+window.addEventListener("DOMContentLoaded", () =>{ //Usa o valor na url para mandar o animal para a função de escolha quando a pagina é carregada
+    const url = new URLSearchParams(window.location.search);
+    escolhe(url.get('animal'));
+})
 
-function listagemProds(Animal){
-    switch(Animal){
-        case "todos":{
-            
-            break;    
+function escolhe(Animal){// Escolhe a categoria(animal) de produtos a serem mostrados
+    if(Animal != 'todos'){
+        const botao = document.getElementById(Animal);
+        if(botao.classList.contains('active')){
+            botao.classList.remove('active');    
+        }else{
+            botao.classList.add('active'); 
+        }  
         }
-        case "cao":{
-            
-            break;    
+    console.log(Animal);
+    ativados = document.querySelectorAll('.active');
+    ativados.forEach(element => {
+        if(element.id != Animal){
+            element.classList.remove('active'); 
         }
-        case "gato":{
-            
-            break;    
-        }
-        case "ave":{
-            
-            break;    
-        }
-        case "roedor":{
-            
-            break;    
-        }
-        case "peixe":{
-            
-            break;    
-        }
-    }
+    });
 }
