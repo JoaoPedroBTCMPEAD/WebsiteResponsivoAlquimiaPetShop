5fe9884bf3d08a49cb0be3849d56414b475b761b
João Pedro <joao.26100698ead@alunos.unisagrado.edu.br>
2026-09-15 00:35:05 -0300

    Inicio de preparo para preencher listagem de produtos

--- js/prods.js
+++ js/prods.js
@@ -4,7 +4,25 @@ function produtos(escolha){ //Usado em index.html para escolher o animal
     urlMod.searchParams.set("animal", escolha);
     window.location.href = urlMod.href;
 } 
-
+var listAnimais = 
+    [
+    'todos'
+    ,'cao'
+    ,'gato'
+    ,'ave'
+    ,'roedor'
+    ,'peixe'
+    ];
+var listTitulos = 
+    [
+    'Produtos para todos'
+    ,'Produtos para cães'
+    ,'Produtos para gatos'
+    ,'Produtos para aves'
+    ,'Produtos para roedores'
+    ,'Produtos para peixes'
+    ];
+var presSelec;
 function retornaInicio(){
     window.location.href = "../"
 }
@@ -17,12 +35,19 @@ function escolhe(Animal){// Escolhe a categoria(animal) de produtos a serem most
     if(Animal != 'todos'){
         const botao = document.getElementById(Animal);
         if(botao.classList.contains('active')){
-            botao.classList.remove('active');    
+            botao.classList.remove('active');  
+            Animal ='todos'  
         }else{
             botao.classList.add('active'); 
         }  
+    }
+    listAnimais.forEach((anm, i) =>{
+        if(anm == Animal){
+            presSelec = i;
+            (document.getElementById('preencher').innerHTML = listTitulos[i])
         }
-    console.log(Animal);
+    })
+    console.log(Animal);//Tirar depois do teste
     ativados = document.querySelectorAll('.active');
     ativados.forEach(element => {
         if(element.id != Animal){
